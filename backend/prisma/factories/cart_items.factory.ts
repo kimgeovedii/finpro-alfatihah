1d0420a1916064c31b72ccdd0c93ab4e5ba5e726
FlazeFy2 <79736302+FlazeFy@users.noreply.github.com>
2026-04-07 22:41:01 +0700

    feat(database) : adjust business relational concept between product and cart / order related table

--- backend/prisma/factories/cart_items.factory.ts
+++ backend/prisma/factories/cart_items.factory.ts
@@ -1,13 +1,15 @@
 import { faker } from "@faker-js/faker"
 import { prisma } from "../../src/config/prisma"
-
+import { ProductRepository } from "../../src/features/products/repositories/product.repository"
 import { CartRepository } from "../../src/features/carts/repositories/cart.repository"
 
 class CartItemsFactory {
     private cartRepository: CartRepository
+    private productRepository: ProductRepository
 
     constructor(){
         this.cartRepository = new CartRepository()
+        this.productRepository = new ProductRepository()
     }
 
     public create = async () => {
@@ -15,10 +17,15 @@ class CartItemsFactory {
         const cart = await this.cartRepository.findRandomCart()
         if (!cart) throw new Error('Cannot create cart items without cart')
 
+        // Get random product from repo
+        const product = await this.productRepository.findRandomProduct(cart.branchId)
+        if (!product) throw new Error('Cannot create cart items without products')
+
         return prisma.cart_items.create({
             data: {
                 id: faker.string.uuid(),
                 cartId: cart.id,
+                productId: product.id,
                 quantity: faker.number.int({ min: 1, max: 5 }),
                 createdAt: faker.date.past({ years: 1 }),
             },
--- backend/prisma/factories/order_items.factory.ts
+++ backend/prisma/factories/order_items.factory.ts
@@ -2,12 +2,15 @@ import { faker } from "@faker-js/faker"
 import { prisma } from "../../src/config/prisma"
 
 import { OrderRepository } from "../../src/features/orders/repositories/order.repository"
+import { ProductRepository } from "../../src/features/products/repositories/product.repository"
 
 class OrderItemsFactory {
     private orderRepository: OrderRepository
+    private productRepository: ProductRepository
 
     constructor(){
         this.orderRepository = new OrderRepository()
+        this.productRepository = new ProductRepository()
     }
 
     public create = async () => {
@@ -15,10 +18,15 @@ class OrderItemsFactory {
         const order = await this.orderRepository.findRandomOrder()
         if (!order) throw new Error('Cannot create order items without order')
 
+        // Get random product from repo
+        const product = await this.productRepository.findRandomProduct(order.branchId)
+        if (!product) throw new Error('Cannot create cart items without products')
+
         return prisma.order_items.create({
             data: {
                 id: faker.string.uuid(),
                 orderId: order.id,
+                productId: product.id,
                 price: faker.number.int({ min: 50000, max: 400000 }), // for now
                 quantity: faker.number.int({ min: 1, max: 5 })
             },
