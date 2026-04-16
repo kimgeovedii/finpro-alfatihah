8bde695c9942bb9857fba5a01d81a2534b4a963e
FlazeFy2 <flazen.edu@gmail.com>
2026-04-16 09:25:43 +0700

    docs(payment) : add API doc for POST add manual payment evidence

--- backend/src/constants/business.const.ts
+++ backend/src/constants/business.const.ts
@@ -9,6 +9,6 @@ export const orderCode: string = 'ORD'
 export const weightGramsShippingDefault: number = 1000 // 1 kg
 export const courierShippingDefault: string = 'jne' 
 // Multer & Cloudinary : File handling
-export const maxSizePaymentEvidence: number = 2 * 1024 * 1024
+export const maxSizePaymentEvidence: number = 2 * 1024 * 1024 // 2 mb
 export const allowedMimeTypesPaymentEvidence: string[] = ["image/jpeg", "image/png", "image/jpg"]
 export const allowedExtensionsPaymentEvidence = [".jpg", ".jpeg", ".png"]
