baef80b48b69023730ce66d9cd62385fc7744c4f
Kimgeovedii <mohamadmustofahakim@gmail.com>
2026-04-26 00:23:26 +0700

    feat: implement dashboard layout and core homepage features including interactive store mapping

--- frontend/src/features/Homepage/HomePageView.tsx
+++ frontend/src/features/Homepage/HomePageView.tsx
@@ -8,19 +8,15 @@ import { ExclusiveVoucher } from "./ExclusiveVoucher";
 import { NotificationHandler } from "../../components/common/NotificationHandler";
 import { Suspense } from "react";
 
-export const LandingPageView = () => {
+export const HomePageView = () => {
   return (
-    <div className="bg-surface min-h-screen">
+    <>
       <Suspense><NotificationHandler /></Suspense>
-      <MainNavbar />
-      <main className="max-w-7xl mx-auto px-6 lg:px-8 space-y-12 py-8 relative z-10">
         <HeroSection />
         <MapsView />
         <AllowAccess />
         <RecommendationSlide />
         <ExclusiveVoucher />
-      </main>
-      <MainFooter />
-    </div>
+    </>
   );
 };
